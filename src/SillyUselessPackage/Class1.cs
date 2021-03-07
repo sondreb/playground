a86a729981d2795a588f7f8966e8051950185b3e
SondreB <sondre@outlook.com>
2021-03-07 23:24:54 +0100

    Extend the code of the silly library

--- src/SillyUselessPackage/Class1.cs
+++ src/SillyUselessPackage/Class1.cs
@@ -4,5 +4,20 @@ namespace SillyUselessPackage
 {
     public class Class1
     {
+        private string hello = "world";
+
+        public Class1()
+        {
+            hello = "hello";
+        }
+
+        /// <summary>
+        /// Make sure you generate a random number.
+        /// </summary>
+        /// <returns></returns>
+        public int Generate()
+        {
+            return 1;
+        }
     }
 }
